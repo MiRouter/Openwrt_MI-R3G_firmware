a516b42917e490f36068c950e8a770156a5c0d52
liuran001 <32791471+liuran001@users.noreply.github.com>
2020-04-12 18:45:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/feeds/luci/luci-app-adguardhome
+
