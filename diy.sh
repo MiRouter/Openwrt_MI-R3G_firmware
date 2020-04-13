4918b8712f5b3e215247241716497d245c263432
liuran001 <32791471+liuran001@users.noreply.github.com>
2020-04-13 20:34:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,8 +8,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/feeds/luci/luci-app-adguardhome
-git clone https://github.com/Lienol/openwrt-package
-cd package/feeds/luci/
-mv -f /openwrt/openwrt-package/lienol/* .
+cd ..
+git clone https://github.com/Lienol/openwrt-package lede-package/
+cd openwrt/package/feeds/luci/
+mv -f /lede-package/lienol/* .
 ./scripts/feeds update -a
 ./scripts/feeds install -a
