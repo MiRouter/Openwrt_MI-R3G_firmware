63c8a8d382da553a9a3166a1ffe0432a676516ba
liuran001 <32791471+liuran001@users.noreply.github.com>
2020-04-14 20:18:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,5 +9,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/feeds/luci/luci-app-adguardhome
 sed -i 'src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
